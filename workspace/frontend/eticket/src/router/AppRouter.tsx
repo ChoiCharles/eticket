aa1942d75611a5c4f5c10c4e3bc5d210b62e06e0
오영재 <dhdnqkrtk22@naver.com>
2023-09-08 17:53:58 +0900

    feat(ui): create TOP & BOTTOM navigation bar

--- workspace/frontend/eticket/src/router/AppRouter.tsx
+++ workspace/frontend/eticket/src/router/AppRouter.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home.tsx';
+import Home from '../pages/Home/Home.tsx';
 import Test from '../pages/Test.tsx';
 
 function AppRouter() {
