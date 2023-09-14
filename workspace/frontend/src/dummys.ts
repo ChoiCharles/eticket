e722a1706a6fe493769c8aba0d9e0f6b29a594cb
GoldGyu <lkg2097@naver.com>
2023-09-14 10:27:58 +0900

    chore: frontend dummy-data update

--- workspace/frontend/src/dummys.ts
+++ workspace/frontend/src/dummys.ts
@@ -1,41 +1,43 @@
+import Img from 'assets/memphis.svg';
+
 const dummyConcerts = [
   {
     id: 0,
-    image: '이미지입니다.',
+    image: Img,
     title: '오페라의 유령',
     location: '광주광역시 광산구 하남로 삼성전자 광주사업장',
-    date: '2023-09-08',
+    date: '2023-09-08 ~ 2023-09-10',
   },
   {
     id: 1,
-    image: '이미지2입니다.',
+    image: Img,
     title: '다른 공연 제목',
     location: '다른 장소',
-    date: '2023-09-09',
+    date: '2023-09-09 ~ 2023-09-11',
   },
 
   {
     id: 2,
-    image: '이미지3입니다.',
+    image: Img,
     title: '다른 공연 제목2',
     location: '다른 장소2',
-    date: '2023-09-10',
+    date: '2023-09-10 ~ 2023-09-14',
   },
 
   {
     id: 3,
-    image: '이미지4입니다.',
+    image: Img,
     title: '다른 공연 제목3',
     location: '다른 장소3',
-    date: '2023-09-11',
+    date: '2023-09-11 ~ 2023-09-12',
   },
 
   {
     id: 10,
-    image: '이미지11입니다.',
+    image: Img,
     title: '다른 공연 제목10',
     location: '다른 장소10',
-    date: '2023-09-18',
+    date: '2023-09-18 ~ 2023-09-15',
   },
 ];
 
