ddff56a8b69bc3c5bb06c6edb76b76a8c03375d4
유연석 <ysyoo627@naver.com>
2023-09-15 11:11:56 +0900

    fix(backend): fix Sample Code Of Searching Performances API

--- workspace/backend/src/main/java/org/oao/eticket/application/domain/service/SearchPerformancesService.java
+++ workspace/backend/src/main/java/org/oao/eticket/application/domain/service/SearchPerformancesService.java
@@ -1,10 +1,14 @@
 package org.oao.eticket.application.domain.service;
 
+import lombok.RequiredArgsConstructor;
 import org.oao.eticket.application.domain.model.Performance;
 import org.oao.eticket.application.port.in.SearchPerformancesUseCase;
+import org.oao.eticket.common.annotation.UseCase;
 
 import java.util.List;
 
+@UseCase
+@RequiredArgsConstructor
 public class SearchPerformancesService implements SearchPerformancesUseCase {
 
   @Override
