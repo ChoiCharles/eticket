957d5371450ac1e5691d9b7568f3e6b8a5c2d391
GoldGyu <lkg2097@naver.com>
2023-09-18 18:01:03 +0900

    chore: frontend layout setup

--- workspace/frontend/src/components/user/MetamaskLogin/MetamaskLogin.tsx
+++ workspace/frontend/src/components/user/MetamaskLogin/MetamaskLogin.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './MetamaskFrom.scss';
 
-function MetamaskFrom() {
+function MetamaskLogin() {
   return (
     <div>
       <div>Metamask</div>
@@ -12,4 +12,4 @@ function MetamaskFrom() {
   );
 }
 
-export default MetamaskFrom;
+export default MetamaskLogin;
