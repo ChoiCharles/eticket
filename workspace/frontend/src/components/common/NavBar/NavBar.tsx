99b3295dea466de3af58672b5eaaa76184fa6102
오영재 <dhdnqkrtk22@naver.com>
2023-09-19 17:57:02 +0900

    feat(ui) : add drawer(sidebar)

--- workspace/frontend/src/components/common/NavBar/NavBar.tsx
+++ workspace/frontend/src/components/common/NavBar/NavBar.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Buttons from 'components/common/Buttons/Buttons';
 import Logo from 'components/common/Logo/Logo';
+import { Drawer } from '@mui/material';
 
 import './NavBar.scss';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenDrawer: () => void = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseDrawer: () => void = () => {
+    setIsOpen(false);
+  };
   return (
     <div className="nav-bar">
       <Logo />
-      <Buttons />
+      <Buttons handleOpenDrawer={handleOpenDrawer} />
+      <Drawer anchor="right" open={isOpen} onClick={handleCloseDrawer} />
     </div>
   );
 };
