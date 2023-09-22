202106393124db58da585def79106539470d1c39
기대성 <kmeos1579@gmail.com>
2023-09-22 09:54:33 +0900

    fix: prevent full table scan

--- workspace/blockchain-tracker/persistence/generated/block_sync_log.sql.go
+++ workspace/blockchain-tracker/persistence/generated/block_sync_log.sql.go
@@ -10,7 +10,10 @@ import (
 )
 
 const createSyncLog = `-- name: CreateSyncLog :exec
-INSERT INTO block_sync_log (` + "`" + `lower_block` + "`" + `, ` + "`" + `upper_block` + "`" + `) VALUES (?, ?)
+INSERT INTO
+    block_sync_log (` + "`" + `lower_block` + "`" + `, ` + "`" + `upper_block` + "`" + `)
+VALUES
+    (?, ?)
 `
 
 type CreateSyncLogParams struct {
@@ -29,7 +32,7 @@ SELECT
 FROM
     block_sync_log
 ORDER BY
-    ` + "`" + `sync_time` + "`" + ` DESC
+    ` + "`" + `block_sync_id` + "`" + ` DESC
 LIMIT
     1
 `
--- workspace/blockchain-tracker/sql/block_sync_log.sql
+++ workspace/blockchain-tracker/sql/block_sync_log.sql
@@ -4,9 +4,12 @@ SELECT
 FROM
     block_sync_log
 ORDER BY
-    `sync_time` DESC
+    `block_sync_id` DESC
 LIMIT
     1;
 
 -- name: CreateSyncLog :exec
-INSERT INTO block_sync_log (`lower_block`, `upper_block`) VALUES (?, ?);
+INSERT INTO
+    block_sync_log (`lower_block`, `upper_block`)
+VALUES
+    (?, ?);
