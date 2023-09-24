a7c952acde6a37c20ef30122d7ee39be884b3e7c
Gold-gyu <lkg2097@naver.com>
2023-09-24 21:51:29 +0900

    chore: frontend dummydata update

--- workspace/frontend/src/dummys.ts
+++ workspace/frontend/src/dummys.ts
@@ -1,40 +1,92 @@
-import Img from 'assets/memphis.svg';
+// import Img from 'assets/memphis.svg';
 
 const dummyConcerts = [
+  // {
+  //   id: 0,
+  //   image: Img,
+  //   title: '오페라의 유령',
+  //   location: '광주광역시 광산구 하남로 삼성전자',
+  //   date: '2023-09-08 ~ 2023-09-10',
+  // },
   {
     id: 0,
-    image: Img,
-    title: '오페라의 유령',
-    location: '광주광역시 광산구 하남로 삼성전자',
-    date: '2023-09-08 ~ 2023-09-10',
-  },
-  {
-    id: 1,
-    image: Img,
+    image:
+      'https://tickets.interpark.com/contents/_next/image?url=https%3A%2F%2Fticketimage.interpark.com%2FPlay%2Fimage%2Flarge%2F23%2F23013154_p.gif&w=750&q=75',
     title: '다른 공연 제목',
     location: '다른 장소',
     date: '2023-09-09 ~ 2023-09-11',
   },
 
   {
-    id: 2,
-    image: Img,
+    id: 1,
+    image:
+      'https://tickets.interpark.com/contents/_next/image?url=https%3A%2F%2Fticketimage.interpark.com%2FPlay%2Fimage%2Flarge%2F23%2F23011055_p.gif&w=750&q=75',
     title: '다른 공연 제목2',
     location: '다른 장소2',
     date: '2023-09-10 ~ 2023-09-14',
   },
 
   {
+    id: 2,
+    image:
+      'https://tickets.interpark.com/contents/_next/image?url=https%3A%2F%2Fticketimage.interpark.com%2FPlay%2Fimage%2Flarge%2F23%2F23012636_p.gif&w=750&q=75',
+    title: '다른 공연 제목3',
+    location: '다른 장소3',
+    date: '2023-09-11 ~ 2023-09-12',
+  },
+  {
     id: 3,
-    image: Img,
+    image:
+      'https://tickets.interpark.com/contents/_next/image?url=https%3A%2F%2Fticketimage.interpark.com%2FPlay%2Fimage%2Flarge%2F23%2F23013039_p.gif&w=750&q=75',
+    title: '다른 공연 제목3',
+    location: '다른 장소3',
+    date: '2023-09-11 ~ 2023-09-12',
+  },
+  {
+    id: 4,
+    image:
+      'https://tickets.interpark.com/contents/_next/image?url=https%3A%2F%2Fticketimage.interpark.com%2FPlay%2Fimage%2Flarge%2F23%2F23011967_p.gif&w=750&q=75',
+    title: '다른 공연 제목3',
+    location: '다른 장소3',
+    date: '2023-09-11 ~ 2023-09-12',
+  },
+  {
+    id: 5,
+    image:
+      'https://tickets.interpark.com/contents/_next/image?url=https%3A%2F%2Fticketimage.interpark.com%2FPlay%2Fimage%2Flarge%2F23%2F23012802_p.gif&w=750&q=75',
+    title: '다른 공연 제목3',
+    location: '다른 장소3',
+    date: '2023-09-11 ~ 2023-09-12',
+  },
+  {
+    id: 6,
+    image:
+      'https://tickets.interpark.com/contents/_next/image?url=https%3A%2F%2Fticketimage.interpark.com%2FPlay%2Fimage%2Flarge%2FP0%2FP0003604_p.gif&w=750&q=75',
+    title: '다른 공연 제목3',
+    location: '다른 장소3',
+    date: '2023-09-11 ~ 2023-09-12',
+  },
+  {
+    id: 7,
+    image:
+      'https://tickets.interpark.com/contents/_next/image?url=https%3A%2F%2Fticketimage.interpark.com%2FPlay%2Fimage%2Flarge%2F23%2F23011405_p.gif&w=750&q=75',
+    title: '다른 공연 제목3',
+    location: '다른 장소3',
+    date: '2023-09-11 ~ 2023-09-12',
+  },
+  {
+    id: 8,
+    image:
+      'https://tickets.interpark.com/contents/_next/image?url=https%3A%2F%2Fticketimage.interpark.com%2FPlay%2Fimage%2Flarge%2F23%2F23012677_p.gif&w=750&q=75',
     title: '다른 공연 제목3',
     location: '다른 장소3',
     date: '2023-09-11 ~ 2023-09-12',
   },
 
   {
-    id: 10,
-    image: Img,
+    id: 9,
+    image:
+      'https://tickets.interpark.com/contents/_next/image?url=https%3A%2F%2Fticketimage.interpark.com%2FPlay%2Fimage%2Flarge%2F23%2F23013495_p.gif&w=750&q=75',
     title: '다른 공연 제목10',
     location: '다른 장소10',
     date: '2023-09-18 ~ 2023-09-15',
