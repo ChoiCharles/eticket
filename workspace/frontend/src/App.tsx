4b3dd7802ddfa78ed836171bd6088ecc9def3769
오영재 <dhdnqkrtk22@naver.com>
2023-09-25 15:40:28 +0900

    fix(frontend) : replace fail to checkout page and add success component

--- workspace/frontend/src/App.tsx
+++ workspace/frontend/src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import AppRouter from './router/AppRouter.tsx';
 
 function App() {
-  return <AppRouter />;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#80C0C0',
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <AppRouter />
+    </ThemeProvider>
+  );
 }
 
 export default App;
