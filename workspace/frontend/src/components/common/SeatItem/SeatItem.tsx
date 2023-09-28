019e13a6881dfb517771b7f0a9834dc3189ccabc
GoldGyu <lkg2097@naver.com>
2023-09-28 16:29:29 +0900

    feat: frontend section & seat reservation ui/ux

--- workspace/frontend/src/components/common/SeatItem/SeatItem.tsx
+++ workspace/frontend/src/components/common/SeatItem/SeatItem.tsx
@@ -8,9 +8,9 @@ import useMovePage from 'hooks/useMovePage';
 
 function SeatItem({ index }: { index: number }) {
   const { movePage } = useMovePage();
-
+  // const [totalPrice, setTotalPrice] = useState(0);
   const clickBuyBtn = () => {
-    movePage('/', null);
+    movePage('/checkout', null);
   };
 
   const data = SeatDummy[index].seat;
@@ -37,7 +37,7 @@ function SeatItem({ index }: { index: number }) {
               // eslint-disable-next-line react/no-array-index-key
               <div key={i} className="select-seat-price-box">
                 <div className="selected-seat">
-                  <div>{index}섹션</div>
+                  {/* <div>{index}섹션</div> */}
                   <div>{seat + 1}번 좌석</div>
                 </div>
                 <div> 가격: 120,000원</div>
@@ -47,9 +47,15 @@ function SeatItem({ index }: { index: number }) {
         ) : (
           <div className="no-selected-seats">선택한 좌석이 없습니다.</div>
         )}
+
         {selectedSeats.length > 0 && (
-          <div className="click-buy-button" onClick={clickBuyBtn} aria-hidden>
-            결제하기
+          <div>
+            <div className="total-price">
+              합계: {selectedSeats.length * 120000}원
+            </div>
+            <div className="click-buy-button" onClick={clickBuyBtn} aria-hidden>
+              결제하기
+            </div>
           </div>
         )}
       </div>
