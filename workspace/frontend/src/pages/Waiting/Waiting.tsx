d590305ab3af92ae280a3272ca51f39b8acd7b18
오영재 <dhdnqkrtk22@naver.com>
2023-10-03 23:51:40 +0900

    feat(frontend) : add post and delete waiting api

--- workspace/frontend/src/pages/Waiting/Waiting.tsx
+++ workspace/frontend/src/pages/Waiting/Waiting.tsx
@@ -7,20 +7,9 @@ import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import { Client } from '@stomp/stompjs';
 
 import Captcha from 'components/common/Captcha/Captcha';
+import instance from 'apis/utils/instance';
 
 const URL = `wss://${window.location.origin.split('//')[1]}/ws`;
-
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  width: '70%',
-  transform: 'translate(-50%, -50%)',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
 
 const Waiting = () => {
   const [order, setOrder] = useState<number | null>(null);
@@ -32,88 +21,110 @@ const Waiting = () => {
   const [openCaptcha, setOpenCaptcha] = useState<boolean>(true);
 
   useEffect(() => {
+    const requestBody = {
+      userId: 0,
+      performanceScheduleId: 2,
+    };
+    instance
+      .post('/api/waiting', requestBody)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
     if (openCaptcha === false) {
       const client = new Client({
         brokerURL: URL,
         reconnectDelay: 5000,
         debug: str => console.log(str),
       });
-  
+
       client.onConnect = () => {
         setConnected(true);
         setStompClient(client);
-  
-        client.subscribe('/queue', response => {
+
+        client.subscribe('/sub', response => {
           const message = JSON.parse(response.body);
           console.log(message);
           setOrder(Number(message));
-  
+
           if (order !== null && order <= 0) {
             movePage(`/seat/${waitingId}/${dateId}`, null);
           }
         });
       };
-  
+
       client.onDisconnect = () => {
         console.log(connected);
         console.log(stompClient);
         setConnected(false);
         setStompClient(null);
       };
-  
+
       client.activate();
-  
+
       return () => {
         client.deactivate();
       };
     }
+    return () => {
+      instance
+        .delete('/api/ticketing', requestBody)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+    };
   }, [openCaptcha]);
 
   return (
     <>
-        <div>
-          <BackNavBar title="" />
-          <Modal open={openCaptcha}>
-            <Box sx={style}>
-              <Captcha setOpenCaptcha={setOpenCaptcha}/>
-            </Box>
-          </Modal>
-          <Box
-            sx={{
-              mt: '150px',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <AccessTimeIcon fontSize="large" />
-            <Typography my={2} variant="h5">
-              나의 대기 순서
-            </Typography>
-            <Typography variant="h2">{order}</Typography>
-            <LinearProgress
-              sx={{ width: '80%', height: 20, borderRadius: 2, my: 5 }}
-            />
-            <Typography variant="body1">
-              현재 접속 인원이 많아 대기 중입니다.
-            </Typography>
-            <Typography variant="body1">
-              잠시만 기다려주시면 예매 페이지로 이동합니다.
-            </Typography>
-            <Typography mt={2} variant="body2">
-              <b>⚠주의⚠</b>
-            </Typography>
-            <Typography variant="body2">
-              닫기, 새로고침, 뒤로가기 또는 재접속하시면
-            </Typography>
-            <Typography variant="body2">
-              대기 순서가 초기화되어 대기 시간이 더 길어집니다.
-            </Typography>
-          </Box>
-        </div>
-        
-      
+      <BackNavBar title="" />
+      <Modal open={openCaptcha}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            width: '70%',
+            transform: 'translate(-50%, -50%)',
+            bgcolor: 'background.paper',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Captcha setOpenCaptcha={setOpenCaptcha} />
+        </Box>
+      </Modal>
+      <Box
+        sx={{
+          mt: '150px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <AccessTimeIcon fontSize="large" />
+        <Typography my={2} variant="h5">
+          나의 대기 순서
+        </Typography>
+        <Typography variant="h2">{order}</Typography>
+        <LinearProgress
+          sx={{ width: '80%', height: 20, borderRadius: 2, my: 5 }}
+        />
+        <Typography variant="body1">
+          현재 접속 인원이 많아 대기 중입니다.
+        </Typography>
+        <Typography variant="body1">
+          잠시만 기다려주시면 예매 페이지로 이동합니다.
+        </Typography>
+        <Typography mt={2} variant="body2">
+          <b>⚠주의⚠</b>
+        </Typography>
+        <Typography variant="body2">
+          닫기, 새로고침, 뒤로가기 또는 재접속하시면
+        </Typography>
+        <Typography variant="body2">
+          대기 순서가 초기화되어 대기 시간이 더 길어집니다.
+        </Typography>
+      </Box>
     </>
   );
 };
