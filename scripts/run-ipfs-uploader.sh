b6153b12991e388e2796e87cc5652584907f2fc0
meo-s <kmeos1579@gmail.com>
2023-10-04 03:25:39 +0900

    chore: rename blockchian-ipfs-uploader to eticket-ipfs-uploader

--- scripts/run-ipfs-uploader.sh
+++ scripts/run-ipfs-uploader.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-WORKSPACE_DIR="./workspace"
-DOCKER_COMPOSE_DIR="./docker-compose"
-
-if [[ $(pwd) =~ .+/scripts$ ]]; then
-    cd .. || exit 1
-fi
+MAIN_DIR="$(realpath "$(dirname "$0")"/..)"
+DOCKER_COMPOSE_DIR="$MAIN_DIR/docker-compose"
 
 if [[ $* == *--stop* ]]; then
-    docker-compose -f "$DOCKER_COMPOSE_DIR/services/docker-compose-blockchain-ipfs-uploader.yaml" down
+    docker-compose -f "$DOCKER_COMPOSE_DIR/services/docker-compose-eticket-ipfs-uploader.yaml" down
     exit $?
 fi
 
 if [[ $* != *--no-build* ]]; then
-    if ! sh -c "cd $(realpath $WORKSPACE_DIR)/blockchain-ipfs-uploader && sh ./build.sh"; then
-        echo failed to build blockchain-ipfs-uploader
+    if ! sh -c "$MAIN_DIR/workspace/eticket-ipfs-uploader/scripts/build.sh"; then
+        echo failed to build eticket-ipfs-uploader
         exit 1
     fi
 fi
 
-docker-compose -f "$DOCKER_COMPOSE_DIR/services/docker-compose-blockchain-ipfs-uploader.yaml" down
-docker-compose -f "$DOCKER_COMPOSE_DIR/services/docker-compose-blockchain-ipfs-uploader.yaml" up -d --force-recreate --no-deps --build eticket_ipfs_uploader
+docker-compose -f "$DOCKER_COMPOSE_DIR/services/docker-compose-eticket-ipfs-uploader.yaml" down
+docker-compose -f "$DOCKER_COMPOSE_DIR/services/docker-compose-eticket-ipfs-uploader.yaml" up -d --force-recreate --no-deps --build eticket_ipfs_uploader
 
 if [[ $(docker images --filter "dangling=true" -q | wc -l) != 0 ]]; then
     # shellcheck disable=SC2046
