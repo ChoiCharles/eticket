7828a0814800d283fa18fa1e985545c076d49da3
오영재 <dhdnqkrtk22@naver.com>
2023-10-06 10:02:28 +0900

    chore : change nft detail

--- workspace/frontend/src/components/user/MyPage/MyNFT/NFTDetail.tsx
+++ workspace/frontend/src/components/user/MyPage/MyNFT/NFTDetail.tsx
@@ -1,47 +1,30 @@
 import './NFTDetail.scss';
 import React from 'react';
-import testimage from 'assets/test.jfif';
-// import useMetaData from 'hooks/useMetaData';
-import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import BackNavBar from 'components/common/BackNavBar/BackNavBar';
+import { useLocation } from 'react-router-dom';
 
 function NFTDetail() {
-  // const { metadata } = useMetaData()
-  // const NFTImage = metadata?.image
-  // const NFTName = metadata?.name
-  // const NFTAttributes = metadata?.attributes
+  const { state } = useLocation();
 
   return (
     <div>
       <BackNavBar title="" />
-      <Card sx={{ minHeight: '350px', px: 1 }} elevation={0}>
-        <CardMedia
-          sx={{ minHeight: '350px', borderRadius: 1 }}
-          component="img"
-          image={testimage}
-          alt="img"
-        />
-        <CardContent>
-          <Typography variant="h5">이름 : 내 NFT</Typography>
-          <Typography variant="h5">좌석 : s석 1번</Typography>
-        </CardContent>
-      </Card>
       <div className="NFTContainer-detail">
         <div className="NFTCard-detail">
           <div className="NFTCardImg-detail">
             <img
-              src={testimage}
-              alt="NFT Image"
+              src={state.image}
+              alt="img"
               style={{ width: '100%', height: '100%' }}
             />
-            {/* <img src={NFTImage} alt="NFT Image" style={{width: '100%', height: '100%'}}/> */}
           </div>
         </div>
         <div className="NFTInfo-detail">
-          <h3>이름 : 내 NFT</h3>
-          <h3>좌석 : s석 1번</h3>
-          {/* <h3>이름 : {NFTName}</h3>
-          <h3>좌석 : {NFTAttributes[1].value} {NFTAttributes[0].value}</h3> */}
+          <h3>이름 : {state.name}</h3>
+          <h3>
+            좌석 :{' '}
+            {`${state.attributes[1].value}석 ${state.attributes[0].value}번`}
+          </h3>
         </div>
       </div>
     </div>
